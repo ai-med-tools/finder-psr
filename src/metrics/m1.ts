0e04e0ea739eef51b0fbbc7a280381691b2d8595
anton_shev <a.shevtosv@webpractik.ru>
2023-06-01 18:09:03 +0300

    feat(metric): Метрики
    * Расширил вывод метрик M1
    
    Refs: Cu-85zt6pcjq

--- src/metrics/m1.ts
+++ src/metrics/m1.ts
@@ -1,104 +1,136 @@
 import filter from 'lodash/filter';
-import Tokenizer from "@ai-med-tools/tokenizer";
-import * as fs from "fs";
+import Tokenizer from '@ai-med-tools/tokenizer';
+import * as fs from 'fs';
 
 export type TokenDto = {
-    startFromAnamnesis: number;
-    text: string;
-}
+  startFromAnamnesis: number;
+  text: string;
+};
 export class M1 {
-    public static comparingTwoTokinzedMarkups(tokenizedMarkupExpert: any, tokenizedMarkupMember: any, debugFileName: string| null = null) {
-        // console.log(123);
-        // console.log(debugFileName);
-        if (debugFileName) {
-            // console.log({tokenizedMarkupExpert});
-        }
-
-        const preparedExpertsTokenized = Tokenizer.prepareToTokenize(tokenizedMarkupExpert);
-        const resultExpertsTokenized = Tokenizer.tokenize(preparedExpertsTokenized).flat();
-        const preparedMemberTokenized = Tokenizer.prepareToTokenize(tokenizedMarkupMember);
-        const resultMemberTokenized = Tokenizer.tokenize(preparedMemberTokenized).flat();
-
-        if (debugFileName) {
-            fs.writeFileSync(`src/debug/${debugFileName}.log`, JSON.stringify(resultExpertsTokenized, null, 4));
-        }
-
-
-
-        const viewedExpert = resultExpertsTokenized.map((value) => {
-            return {startFromAnamnesis: value.startFromAnamnesis, text: value.text, viewed: false, right: false}
-        });
-        const viewedMember = resultMemberTokenized.map((value) => {
-            return {startFromAnamnesis: value.startFromAnamnesis, text: value.text, viewed: false, right: false}
-        });
-
-        viewedExpert.map((viewedExpertValue, viewedExpertIndex, viewedExpertArray) => {
-            viewedMember.map((viewedMemberValue, viewedMemberIndex, viewedMemberArray) => {
-                // console.log(viewedMemberValue)
-                if (!viewedMemberValue.viewed) {
-                    if (viewedExpertValue.text == viewedMemberValue.text &&
-                        viewedExpertValue.startFromAnamnesis == viewedMemberValue.startFromAnamnesis) {
-
-                        viewedMemberArray[viewedMemberIndex].right = true;
-                        viewedExpertArray[viewedExpertIndex].right = true;
-                        viewedMemberArray[viewedMemberIndex].viewed = true;
-                        viewedExpertArray[viewedExpertIndex].viewed = true;
-                        return;
-                    }
-
-                }
-            });
-
-        });
-
-        const filteredExpertByOverlap = filter(viewedExpert, (item) => {
-            return item.right === true;
-        });
-        const filteredMemberByOverlap = filter(viewedMember, (item) => {
-            return item.right === true;
-        });
-
-        // console.log(viewedExpert.length);
-        // console.log(filteredExpertByOverlap.length);
-        /** Проверка на существование фильтеред */
-        const accuracy = filteredExpertByOverlap.length / viewedExpert.length;
-        const fullness = filteredMemberByOverlap.length / viewedMember.length;
-
-        const result = (2 * accuracy * fullness) / (accuracy + fullness);
-
-        // console.log(result);
-
-        return result;
+  public static comparingTwoTokinzedMarkups(
+    tokenizedMarkupExpert: any,
+    tokenizedMarkupMember: any,
+    debugFileName: string | null = null,
+  ) {
+    // console.log(123);
+    // console.log(debugFileName);
+    if (debugFileName) {
+      // console.log({tokenizedMarkupExpert});
     }
+
+    const preparedExpertsTokenized = Tokenizer.prepareToTokenize(
+      tokenizedMarkupExpert,
+    );
+    const resultExpertsTokenized = Tokenizer.tokenize(
+      preparedExpertsTokenized,
+    ).flat();
+    const preparedMemberTokenized = Tokenizer.prepareToTokenize(
+      tokenizedMarkupMember,
+    );
+    const resultMemberTokenized = Tokenizer.tokenize(
+      preparedMemberTokenized,
+    ).flat();
+
+    if (debugFileName) {
+      fs.writeFileSync(
+        `src/debug/${debugFileName}.log`,
+        JSON.stringify(resultExpertsTokenized, null, 4),
+      );
+    }
+
+    const viewedExpert = resultExpertsTokenized.map((value) => {
+      return {
+        startFromAnamnesis: value.startFromAnamnesis,
+        text: value.text,
+        viewed: false,
+        right: false,
+      };
+    });
+    const viewedMember = resultMemberTokenized.map((value) => {
+      return {
+        startFromAnamnesis: value.startFromAnamnesis,
+        text: value.text,
+        viewed: false,
+        right: false,
+      };
+    });
+
+    viewedExpert.map(
+      (viewedExpertValue, viewedExpertIndex, viewedExpertArray) => {
+        viewedMember.map(
+          (viewedMemberValue, viewedMemberIndex, viewedMemberArray) => {
+            // console.log(viewedMemberValue)
+            if (!viewedMemberValue.viewed) {
+              if (
+                viewedExpertValue.text == viewedMemberValue.text &&
+                viewedExpertValue.startFromAnamnesis ==
+                  viewedMemberValue.startFromAnamnesis
+              ) {
+                viewedMemberArray[viewedMemberIndex].right = true;
+                viewedExpertArray[viewedExpertIndex].right = true;
+                viewedMemberArray[viewedMemberIndex].viewed = true;
+                viewedExpertArray[viewedExpertIndex].viewed = true;
+                return;
+              }
+            }
+          },
+        );
+      },
+    );
+
+    const filteredExpertByOverlap = filter(viewedExpert, (item) => {
+      return item.right === true;
+    });
+    const filteredMemberByOverlap = filter(viewedMember, (item) => {
+      return item.right === true;
+    });
+
+    // console.log(viewedExpert.length);
+    // console.log(filteredExpertByOverlap.length);
+    /** Проверка на существование фильтеред */
+    const accuracy = filteredExpertByOverlap.length / viewedExpert.length;
+    const completeness = filteredMemberByOverlap.length / viewedMember.length;
+
+    const aggregationOfAccuracyCompleteness =
+      (2 * accuracy * completeness) / (accuracy + completeness);
+
+    // console.log(result);
+
+    return {
+      aggregationOfAccuracyCompleteness,
+      accuracy,
+      completeness,
+    };
+  }
 }
 
-const x =   [
-    {
-        "startFromAnamnesis": 2,
-        "text": "АД"
-    },
-    {
-        "startFromAnamnesis": 2,
-        "text": "АД"
-    },
-    {
-        "startFromAnamnesis": 6,
-        "text": "130/90"
-    }
+const x = [
+  {
+    startFromAnamnesis: 2,
+    text: 'АД',
+  },
+  {
+    startFromAnamnesis: 2,
+    text: 'АД',
+  },
+  {
+    startFromAnamnesis: 6,
+    text: '130/90',
+  },
 ];
-const y =   [
-    {
-        "startFromAnamnesis": 2,
-        "text": "АД"
-    },
-    {
-        "startFromAnamnesis": 6,
-        "text": "130/90"
-    },
-    {
-        "startFromAnamnesis": 6,
-        "text": "222222222"
-    }
+const y = [
+  {
+    startFromAnamnesis: 2,
+    text: 'АД',
+  },
+  {
+    startFromAnamnesis: 6,
+    text: '130/90',
+  },
+  {
+    startFromAnamnesis: 6,
+    text: '222222222',
+  },
 ];
 
 // const result = M1.comparingTwoTokinzedMarkups(x, y);
--- src/metrics/m2.ts
+++ src/metrics/m2.ts
@@ -1,63 +1,74 @@
-import { HungarianAlgWithloss } from "../hungarian.alg.withloss";
-import Tokenizer from "@ai-med-tools/tokenizer";
-import {M1} from "./m1";
+import { HungarianAlgWithloss } from '../hungarian.alg.withloss';
+import Tokenizer from '@ai-med-tools/tokenizer';
+import { M1 } from './m1';
 
 export class M2 {
-    public static neededKeys = ['start', 'end'];
-    public static calcAccuracyOfLocalizationOfSymptoms(expertMarkup: any[], memberMarkup: any[]) {
-        expertMarkup.map((value) => {
-            /** Объект из решения */
-            const keys = Object.keys(value);
-            if (!this.neededKeys.every(item => keys.includes(item))) {
-                throw new Error('Объект экспертной разметки не готов к расчёту');
-            }
-        });
+  public static neededKeys = ['start', 'end'];
+  public static calcAccuracyOfLocalizationOfSymptoms(
+    expertMarkup: any[],
+    memberMarkup: any[],
+  ) {
+    expertMarkup.map((value) => {
+      /** Объект из решения */
+      const keys = Object.keys(value);
+      if (!this.neededKeys.every((item) => keys.includes(item))) {
+        throw new Error('Объект экспертной разметки не готов к расчёту');
+      }
+    });
 
-        memberMarkup.map((value) => {
-            /** Объект из решения */
-            const keys = Object.keys(value);
-            if (!this.neededKeys.every(item => keys.includes(item))) {
-                throw new Error('Объект разметки участника не готов к расчёту');
-            }
-        });
+    memberMarkup.map((value) => {
+      /** Объект из решения */
+      const keys = Object.keys(value);
+      if (!this.neededKeys.every((item) => keys.includes(item))) {
+        throw new Error('Объект разметки участника не готов к расчёту');
+      }
+    });
 
-        // console.log({ xLength: expertMarkup.length} );
-        // console.log({ yLength: memberMarkup.length});
+    // console.log({ xLength: expertMarkup.length} );
+    // console.log({ yLength: memberMarkup.length});
 
-        /** Считаем парасочетания */
+    /** Считаем парасочетания */
 
-        const algStarter = new HungarianAlgWithloss(expertMarkup, memberMarkup, true);
-        const algStarterResult = algStarter.complexSearchDetailSelections();
-        const matchedResult = algStarterResult.getMatchedDetailSelections();
+    const algStarter = new HungarianAlgWithloss(
+      expertMarkup,
+      memberMarkup,
+      true,
+    );
+    const algStarterResult = algStarter.complexSearchDetailSelections();
+    const matchedResult = algStarterResult.getMatchedDetailSelections();
 
-        /** Венгерский алгоритм отдаёт ответы парами - массивы в массиве */
-        /** Элемент с индексом 0 - эксперт */
-        /** Элемент с индексом 1 - участник */
+    /** Венгерский алгоритм отдаёт ответы парами - массивы в массиве */
+    /** Элемент с индексом 0 - эксперт */
+    /** Элемент с индексом 1 - участник */
 
-        const arExpertsFragments = [];
-        const arMemberFragments = [];
+    const arExpertsFragments = [];
+    const arMemberFragments = [];
 
-        for (const resultIter of matchedResult) {
-            arExpertsFragments.push(resultIter[0]);
-            arMemberFragments.push(resultIter[1]);
-        }
-
-        const preparedExpertsTokenized = Tokenizer.prepareToTokenize(arExpertsFragments);
-        const resultExpertsTokenized = Tokenizer.tokenize(preparedExpertsTokenized).flat();
-        const preparedMemberTokenized = Tokenizer.prepareToTokenize(arMemberFragments);
-        const resultMemberTokenized = Tokenizer.tokenize(preparedMemberTokenized).flat();
-
-        /** Формируем G`, находя разницу в длине между длиной G и длиной разметок икс и игрек */
-
-        const xToG = memberMarkup.length - matchedResult.length;
-        const yToG = expertMarkup.length - matchedResult.length;
-
-        const delimeter = xToG + yToG + matchedResult.length;
-
-
-        const m1Result = M1.comparingTwoTokinzedMarkups(arMemberFragments, arExpertsFragments);
-        const result = m1Result / delimeter;
-
-        return result;
+    for (const resultIter of matchedResult) {
+      arExpertsFragments.push(resultIter[0]);
+      arMemberFragments.push(resultIter[1]);
     }
+
+    const preparedExpertsTokenized =
+      Tokenizer.prepareToTokenize(arExpertsFragments);
+    const resultExpertsTokenized = Tokenizer.tokenize(
+      preparedExpertsTokenized,
+    ).flat();
+    const preparedMemberTokenized =
+      Tokenizer.prepareToTokenize(arMemberFragments);
+    const resultMemberTokenized = Tokenizer.tokenize(
+      preparedMemberTokenized,
+    ).flat();
+
+    /** Формируем G`, находя разницу в длине между длиной G и длиной разметок икс и игрек */
+
+    const xToG = memberMarkup.length - matchedResult.length;
+    const yToG = expertMarkup.length - matchedResult.length;
+
+    const delimeter = xToG + yToG + matchedResult.length;
+
+    const { aggregationOfAccuracyCompleteness } =
+      M1.comparingTwoTokinzedMarkups(arMemberFragments, arExpertsFragments);
+    return aggregationOfAccuracyCompleteness / delimeter;
+  }
 }
